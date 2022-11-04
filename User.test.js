0437741144b73044f81df7bc512d9b76da87a657
07GAshog <113988763+07GAshog@users.noreply.github.com>
2022-11-04 16:04:05 +0000

    Probably finished

--- User.test.js
+++ User.test.js
@@ -136,6 +136,30 @@ describe("Testing if User db works", () => {
         
         expect(allBoardsFromUser[0].Boards[0].id).toBe(1)
     })
+
+    test("Testing if Cheese is loading properly with eager loading", async () => {
+        await db.sync({force: true})
+
+        cheeses = await Cheese.bulkCreate([
+            {title: "String Cheese", description: "Cheesy"},
+            {title: "Swiss cheese", description: "more cheese = more hole; more hole = less cheese: more cheese = less cheese"},
+            {title: "Grilled Cheese", description: "The best type"}
+        ])
+    
+        boards = await Board.bulkCreate([
+            {type: "Board",  description: "Woody board",rating: " 5"},
+            {type: "Glass Board", description: "See through",rating: " 7"},
+            {type: "Plastic Board", description: "Oceans worst enemy",rating: " 3"}
+        ])
+    
+        await boards[0].addCheese(cheeses[0])
+        await cheeses[1].addBoard(boards[2])
+        await boards[1].addCheeses(cheeses)
+    
+        const everBoardFromCheese = await Cheese.findAll({include: Board})
+        
+        expect(everBoardFromCheese[1].Boards[1].type).toBe("Glass Board")
+    })
     
 })
 // npm run test 
--- seed.js
+++ seed.js
@@ -3,8 +3,27 @@ const  db  = require("./db")
 
 
 async function seed() {
+    await db.sync({force: true})
 
+    cheeses = await Cheese.bulkCreate([
+        {title: "String Cheese", description: "Cheesy"},
+        {title: "Swiss cheese", description: "more cheese = more hole; more hole = less cheese: more cheese = less cheese"},
+        {title: "Grilled Cheese", description: "The best type"}
+    ])
+
+    boards = await Board.bulkCreate([
+        {type: "Board",  description: "Woody board",rating: " 5"},
+        {type: "Glass Board", description: "See through",rating: " 7"},
+        {type: "Plastic Board", description: "Oceans worst enemy",rating: " 3"}
+    ])
+
+    await boards[0].addCheese(cheeses[0])
+    await cheeses[1].addBoard(boards[2])
+    await boards[1].addCheeses(cheeses)
+
+    const everBoardFromCheese = await Cheese.findAll({include: Board})
     
+    console.log(verBoardFromCheese[1].Boards[1].type)
 }
 
 seed()
